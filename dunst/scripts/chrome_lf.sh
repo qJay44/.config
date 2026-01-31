b74f19981abdbe6ed5ded817461ce1865597e722
qJay44 <08gerkules08@gmail.com>
2026-01-31 03:53:19 +0300

    upd

--- dunst/scripts/chrome_lf.sh
+++ dunst/scripts/chrome_lf.sh
@@ -5,11 +5,11 @@ ARTIST=$(echo "$DUNST_BODY" | awk -v RS='\n\n' 'NR==2 {print $0}' | head -n 1)
 LAST_LINE=$(echo "$DUNST_BODY" | tail -n 1)
 SCROBBLES=$(echo "$LAST_LINE" | grep -o '[0-9]\+')
 
-SUMMARY=" Spotify"
+APP="${${(f)DUNST_BODY}[1]}"
 BODY="<big>$TITLE<span fgcolor='#FFE756'> ($SCROBBLES)</span></big>\n<span fgcolor='#c0c0c0'>$ARTIST</span>"
 
 local -a icon_cmd
 [[ -n "$DUNST_ICON_PATH" ]] && icon_cmd=(-I "$DUNST_ICON_PATH")
 
-dunstify -t 10000 -r $DUNST_ID -a "chrome_spotify_after" "${icon_cmd[@]}" "$SUMMARY" "$BODY"
+dunstify -r $DUNST_ID -a "Last.fm scrobbler" "${icon_cmd[@]}" "$APP" "$BODY"
 
